dfd2f05b9df210f057d394ca612c9605823f2768
Tünzalə <114104944+Tunzale1@users.noreply.github.com>
2022-11-29 03:40:29 -0800

    Add files via upload
    spiderman

---
+++ haqq/src/App.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='body'>
+        <div className='ag'>
+          <div className='logo'>
+        <i className="fa-brands fa-facebook"></i> 
+        <i className="fa-brands fa-twitter"></i>
+        <i className="fa-brands fa-pinterest"></i>
+        <i className="fa-brands fa-telegram"></i>
+        </div>
+        <div className='yah'>
+        <i class="fa-solid fa-location-dot"></i> <p>
+        203 Madison Ave, NY, USA<br></br>
+<b>ADDRESS</b>
+        </p>
+        <i class="fa-regular fa-clock"></i> <p>
+        Monday - Friday 9am - 6pm <br></br>
+<b>TIMEING</b>
+        </p>
+        <i class="fa-regular fa-envelope"></i><p>envato@gmail.com
+<br></br><b>MAIL TO US</b></p>
+</div>
+</div>
+
+<div className='goy'>
+  <div className='tunu'>
+  <img 
+        src="https://themecraze.net/html/volia/images/logo.png"
+        alt="car"
+      />
+  <ul>   
+ <li>Home</li>
+ <li>About</li>
+ <li>Pages</li>
+ <li>Shop</li>
+ <li>Blog</li>
+ <li>Contact us</li>
+ </ul>
+ 
+<i class="fa-solid fa-magnifying-glass"></i>
+<p className='tick'>Book ticket</p>
+</div>
+</div>
+
+<div className='orta'>
+<h3>5DAYS - 15TALK - 2PARTIES</h3>
+<h1>World Digital <br></br>Confrence</h1>
+<div className='enar'>
+<i class="fa-solid fa-location-arrow"></i> <p className='hal'>California, USA</p>
+<i class="fa-solid fa-calendar"></i><p className='hal'>January 20 To 25, 2022</p>
+</div>
+<div className='sukur'>
+  <p className='suk'> Book Ticket</p>
+  <p className='su'> Video Demo</p>
+</div>
+</div>
+</div>
+
+{/* ikinci sehife */}
+<div className='iki'>
+<p className='roz'><b> Our Features </b></p>
+<h2>Features For Our Client <br></br> For Your Event</h2>
+<i class="fa-solid fa-ellipsis"></i>
+
+<div className='dord'>
+
+  <div className='first'>
+  <i class="fa-solid fa-certificate"></i>
+<h3 className='bas'>Best Digital Ideas</h3>
+<p className='ort'>Dolor sit amet consectetur<br></br>elit sed do eiusmod tempor<br></br> incd.</p>
+<p className='read'><b>Read More</b></p> 
+
+  </div>
+<hr></hr>
+  <div className='first'>
+  <i class="fa-solid fa-people-line"></i>
+<h3 className='bas'>Networking People</h3>
+<p className='ort'>Dolor sit amet consectetur<br></br>elit sed do eiusmod tempor<br></br> incd.</p>
+<p className='read'><b>Read More</b></p> 
+
+  </div>
+  <hr></hr>
+  <div className='first'>
+  <i class="fa-regular fa-lightbulb"></i>
+<h3 className='bas'>Inspiring Keynotes</h3>
+<p className='ort'>Dolor sit amet consectetur<br></br>elit sed do eiusmod tempor<br></br> incd.</p>
+<p className='read'><b>Read More</b></p> 
+
+  </div>
+  <hr></hr>
+  <div className='first'>
+  <i class="fa-solid fa-certificate"></i>
+<h3 className='bas'>Confirm Speakers</h3>
+<p className='ort'>Dolor sit amet consectetur<br></br>elit sed do eiusmod tempor<br></br> incd.</p>
+<p className='read'><b>Read More</b></p> 
+
+  </div>
+
+ 
+</div>
+</div>
+      </div>
+    )
+  }
+}
